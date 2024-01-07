a0ddd5140f5540ac23a760bbe028a7b3132daf15
hiepdh <hhiep1hk@gmail.com>
2024-01-07 23:08:21 +0700

    change base url

--- frontend/src/ultils/socketApi.js
+++ frontend/src/ultils/socketApi.js
@@ -1,2 +1,2 @@
 // export const BASE_URL_SOCKET = 'ws://26.188.5.187:5000?id=admin_0'
-export const BASE_URL_SOCKET = 'ws://localhost:2411/ws?id=admin_0'
+export const BASE_URL_SOCKET = 'ws://127.0.0.1:2411/ws?id=admin_0'
